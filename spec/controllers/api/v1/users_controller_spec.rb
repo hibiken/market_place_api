b5a1f95ad7c0f1b4813dff1d3c46c446411f2ac4
Ken Hibino <ken.hibino7@gmail.com>
2016-01-31 08:33:37 -0500

    Set Accept Header field and Content-Type Header field in users controller spec

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController do
   before(:each) do
-    request.headers['Accept'] = "application/vnd.marketplace.v1"
+    request.headers['Accept'] = "application/vnd.marketplace.v1, #{Mime::JSON}"
+    request.headers['Content-Type'] = Mime::JSON.to_s
   end
 
   describe "GET #show" do
     before(:each) do
       @user = FactoryGirl.create(:user)
-      get :show, id: @user.id, format: :json
+      get :show, id: @user.id
     end
 
     it "returns the information about a reporter on a hash" do
@@ -25,7 +26,7 @@ RSpec.describe Api::V1::UsersController do
     context "when user is successfully created" do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for(:user)
-        post :create, { user: @user_attributes }, format: :json
+        post :create, { user: @user_attributes }
       end
 
       it "renders the json representation for the user record just created" do
@@ -43,7 +44,7 @@ RSpec.describe Api::V1::UsersController do
         @invalid_user_attributes = { email: "",
                                     password: "12345678",
                                     password_confirmation: "12345678" }
-        post :create, { user: @invalid_user_attributes }, format: :json
+        post :create, { user: @invalid_user_attributes }
       end
 
       it "renders an error json" do
@@ -67,7 +68,7 @@ RSpec.describe Api::V1::UsersController do
       before(:each) do
         @user = FactoryGirl.create(:user)
         patch :update, { id: @user.id,
-                        user: { email: "newaddress@example.com" } }, format: :json
+                        user: { email: "newaddress@example.com" } }
       end
 
       it "renders the json representation of the updated user" do
@@ -84,7 +85,7 @@ RSpec.describe Api::V1::UsersController do
       before(:each) do
         @user = FactoryGirl.create(:user)
         patch :update, { id: @user.id,
-                         user: { email: "bademail.com" } }, format: :json
+                         user: { email: "bademail.com" } }
       end
 
       it "renders n error json" do
@@ -101,7 +102,7 @@ RSpec.describe Api::V1::UsersController do
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryGirl.create(:user)
-      delete :destroy, { id: @user.id }, format: :json
+      delete :destroy, { id: @user.id }
     end
 
     it "responds with HTTP status 204" do
