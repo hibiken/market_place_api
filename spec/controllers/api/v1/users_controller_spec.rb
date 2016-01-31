30ea09be5c3255a35c2dad659cd0b18ce9931c89
Ken Hibino <ken.hibino7@gmail.com>
2016-01-30 22:04:38 -0500

    Adds user#create action and user create endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -16,8 +16,49 @@ RSpec.describe Api::V1::UsersController do
       expect(user_response[:email]).to eq(@user.email)
     end
 
-    it "should respond with HTTP status 200" do
+    it "responds with HTTP status 200" do
       expect(response.status).to eq(200)
+    end
+  end
+
+  describe "POST #create" do
+    context "when user is successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for(:user)
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it "renders the json representation for the user record just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(@user_attributes[:email])
+      end
+
+      it "responds with HTTP status 201" do
+        expect(response.status).to eq(201)
+      end
+    end
+
+    context "when user is not created" do
+      before(:each) do
+        @invalid_user_attributes = { email: "",
+                                    password: "12345678",
+                                    password_confirmation: "12345678" }
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it "renders an error json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders an error json with explanation" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include("can't be blank")
+      end
+
+      it "responds with HTTP status 422" do
+        expect(response.status).to eq(422)
+      end
     end
   end
 end
