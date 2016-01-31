551b41a2aada236298862c8a041a2760830c597c
Ken Hibino <ken.hibino7@gmail.com>
2016-01-31 09:27:35 -0500

    Adds user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,18 @@ RSpec.describe User do
       expect(@user).not_to be_valid
     end
   end
+
+  describe "#generate_authentication_token!" do
+    it "generates a unique token" do
+      allow(Devise).to receive(:friendly_token).and_return("auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eq("auniquetoken123")
+    end
+
+    it "generates another token when one has already been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eq(existing_user.auth_token)
+    end
+  end
 end
