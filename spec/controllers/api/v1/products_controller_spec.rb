af01c2a5e63e1367b67f145de6a06fe586a55eba
Ken Hibino <ken.hibino7@gmail.com>
2016-02-02 06:48:22 -0500

    Add products#update and products#destroy action and endpoint

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -83,4 +83,84 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
       end
     end
   end
+
+  describe "PUT/PATCH #update" do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      @product = FactoryGirl.create(:product, user: @user)
+    end
+
+    context "when successfully updated" do
+      before(:each) do
+        request.headers['Authorization'] = @user.auth_token
+        patch :update, { user_id: @user.id, id: @product.id,
+                         product: { title: "Updated title" } }
+      end
+
+      it "renders the json representation of the updated product" do
+        product_response = json_response
+        expect(product_response[:title]).to eq("Updated title")
+      end
+
+      it "responds with HTTP status 200" do
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context "when user input is invalid" do
+      before(:each) do
+        request.headers['Authorization'] = @user.auth_token
+        patch :update, { user_id: @user.id, id: @product.id,
+                         product: { price: "two hundered" } }
+      end
+
+      it "renders json with error messages" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include("is not a number")
+      end
+
+      it "responds with HTTP status 422" do
+        expect(response.status).to eq(422)
+      end
+    end
+
+    context "when user is not logged in" do
+      before(:each) do
+        request.headers['Authorization'] = nil
+        patch :update, { user_id: @user.id, id: @product.id,
+                         product: { price: 900 } }
+      end
+
+      it "responds with HTTP status 401" do
+        expect(response.status).to eq(401)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    context "when user is loggged in" do
+      before(:each) do
+        @user = FactoryGirl.create(:user)
+        @product = FactoryGirl.create(:product, user: @user)
+        request.headers['Authorization'] = @user.auth_token
+        delete :destroy, { user_id: @user.id, id: @product.id }
+      end
+
+      it "responds with HTTP status 204" do
+        expect(response.status).to eq(204)
+      end
+    end
+
+    context "when user is not logged in" do
+      before(:each) do
+        @user = FactoryGirl.create(:user)
+        @product = FactoryGirl.create(:product, user: @user)
+        delete :destroy, { user_id: @user.id, id: @product.id }
+      end
+
+      it "responds with HTTP status 401" do
+        expect(response.status).to eq(401)
+      end
+    end
+  end
 end
